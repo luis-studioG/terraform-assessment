ddd240d76013f98d0c88a94c0887d8a4b9a19c90
rahulstudiographene <rahul.sachan@studiographene.com>
2023-05-10 14:37:30 +0530

    feat(SGEBLG-207): PR standardisation and husky updated

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style'],
+    ],
+    'scope-enum': [2, 'always'],
+    'scope-empty': [2, 'never'],
+  },
+};
